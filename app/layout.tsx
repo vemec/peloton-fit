ca7be451b20a12581e3eed14cacbb94deb2f9256
Diego Ghersi <ghersidl@gmail.com>
2025-09-05 17:50:36 -0300

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -126,6 +127,7 @@ export default function RootLayout({
             }
           }}
         />
+        <Analytics />
       </body>
     </html>
   );
